acbd863624d652cd5bd355392f32fb0f81f1b073
Kestin Goforth <kgoforth1503@gmail.com>
2015-02-14 21:54:29 -0500

    Sets focus when triggered

--- ScreenBlocker.py
+++ ScreenBlocker.py
@@ -1,6 +1,8 @@
+import ScriptingBridge as sb
 import numpy as np
 import cv2
 import time
+
 
 def blockScreen(screenWidth, screenHeight):
     img = np.zeros((screenHeight, screenWidth), np.uint8) #Create black empty box
@@ -9,11 +11,15 @@ def blockScreen(screenWidth, screenHeight):
 
     drawCenteredText(img, "Julius", cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 60);
 
-    drawCenteredText(img, "Possible Seizure Avoided", cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), screenHeight / 2)
+    drawCenteredText(img, "Seizure-Guard Triggered", cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), screenHeight / 2)
 
     cv2.imshow("Julius", img) #Show the screen
     cv2.moveWindow("Julius", 0, 0) #Move the window to the top left corner
-    cv2.waitKey(3000) #Close the window after 30 seconds
+
+    process = sb.SBApplication.applicationWithBundleIdentifier_('com.apple.systemevents').processes().objectWithName_("Python")
+    process.setFrontmost_(True)
+
+    cv2.waitKey(10000) #Close the window after 30 seconds
 
 def drawCenteredText(img, text, font, scale, color, yPos):
     size = cv2.getTextSize(text, font, scale, 1)
@@ -24,6 +30,6 @@ def drawCenteredText(img, text, font, scale, color, yPos):
     cv2.putText(img, text, ((width - textWidth) / 2, yPos), font, scale, color, 1, cv2.CV_AA)
 
 
-time.sleep(60)
+time.sleep(20)
 
 blockScreen(1440, 900)
