a209a692a0217091f9411dd1ecdfb65b0d8739a3
Christopher Stokes <christopher.stokes@snowjob.ca>
2015-02-14 23:06:56 -0500

    Added 3 functions implemenign the opencv hist comparisson tool to do an overall scan of the video feed and a segmmented scan that checks sets of 100 pixles at a time.

--- cam.py
+++ cam.py
@@ -1,19 +1,81 @@
 import numpy as np
 import cv2
+import cv
 import datetime
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture("4.mov")
+
+def overallScan(img1, img2):
+    
+    # Our operations on the frame come here
+    
+    hist1 = cv2.calcHist([img1],[0],None,[256],[0,256])
+    hist2 = cv2.calcHist([img2],[0],None,[256],[0,256])
+    sc = cv2.compareHist(hist1, hist2, cv.CV_COMP_BHATTACHARYYA)
+    #print(hist1)  
+    if(sc > 0.15):
+        return True
+    else:
+        return False
+    
+def percisesScan(img1, img2):
+    
+    x, y, z = img2.shape
+    histS1 = histS2 = 0
+    count = 0
+    for i in range (0,x):
+        for j in range (0,y):
+            count += 1
+            histS1 += cv2.calcHist([img1[(i,j)]],[0],None,[256],[0,256])
+            histS2 += cv2.calcHist([img2[(i,j)]],[0],None,[256],[0,256])
+            if(count == 100):
+                sc = cv2.compareHist(histS1, histS2, cv.CV_COMP_BHATTACHARYYA)
+                if(sc > 0.4):
+                    return True
+                else:
+                    return False                
+                
 
 while(True):
-    # Capture frame-by-frame
     ret, frame = cap.read()
+    ret, img1 = cap.read()
+    ret, img2 = cap.read()
+    if(img2 is None):
+        break
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    print(overallScan(img1, img2) and percisesScan(img1, img2))
 
-    print gray
+
+    
+    
+    
+    #plt.hist(frame.ravel(),256,[0,256]); plt.show()
+    #img1 = cv2.imread('C:/ICP/buterfly_1.jpg')
+    #img1= cv2.cvtColor(img1,cv.CV_BGR2HSV)
+    #img2 = cv2.imread('C:/ICP/buterfly_0.jpg')
+    #img2= cv2.cvtColor(img2,cv.CV_BGR2HSV)
+    #h = np.zeros((300,256,3))
+    
+    #bins = np.arange(256).reshape(256,1)
+    #color = [ (255,0,0),(0,255,0),(0,0,255) ]
+    
+    #for ch, col in enumerate(color):
+        #hist_item1 = cv2.calcHist([img1],[ch],None,[256],[0,255])
+        #hist_item2 = cv2.calcHist([img2],[ch],None,[256],[0,255])
+        #cv2.normalize(hist_item1,hist_item1,0,255,cv2.NORM_MINMAX)
+        #cv2.normalize(hist_item2,hist_item2,0,255,cv2.NORM_MINMAX)
+        #sc= cv.CompareHist(hist_item1, hist_item2, cv.CV_COMP_CORREL)
+        #printsc
+        #hist=np.int32(np.around(hist_item))
+        #pts = np.column_stack((bins,hist))
+        #cv2.polylines(h,[pts],False,col)
+    
+    #h=np.flipud(h)
+    #cv2.imwrite('C:/hist.png',h)    
+    
+    #print gray
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',img1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
         
